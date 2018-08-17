e438c908c787fe8db388b769b2212654368d58f1
ManotLuijiu <manot_luijiu@hotmail.com>
2018-08-17 20:38:34 +0700

    third commit

--- src/server/web.server.js
+++ src/server/web.server.js
@@ -1,37 +1,34 @@
-import { resolve } from 'url';
-import { rejects } from 'assert';
-
 const express = require('express');
 
 export default class WebServer {
   constructor() {
-    this.app = express()
-    this.app.use(express.static('dist/public'))
+    this.app = express();
+    this.app.use(express.static('dist/public'));
   }
 
-  start () {
+  start() {
     return new Promise((resolve, reject) => {
       try {
         this.server = this.app.listen(3000, function() {
-          resolve(0)
-        })
+          resolve();
+        });
       } catch (e) {
-        console.error(e)
-        reject(e)
+        console.error(e);
+        reject(e);
       }
-    })
+    });
   }
 
-  stop () {
+  stop() {
     return new Promise((resolve, reject) => {
       try {
         this.server.close(() => {
-          resolve(0)
-        })
+          resolve();
+        });
       } catch (e) {
-        console.error(e.message)
-        reject(e)
+        console.error(e.message);
+        reject(e);
       }
-    })
+    });
   }
 }
